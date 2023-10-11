44e34c43efaed94458207bace4f277161d3ac3e1
benrob13 <144270914+benrob13@users.noreply.github.com>
2023-10-11 13:40:18 -0700

    Add files via upload

--- Working exSEQ Code/Python_Interpreter.ino
+++ Working exSEQ Code/Python_Interpreter.ino
@@ -1,12 +1,12 @@
 #include <Servo.h>
 
-Servo myservo; // Create a servo object
-int pos = 135; // Initial servo position
+Servo myservo;  // Create a servo object
+int pos = 90;   // Initial servo position
 unsigned long movementStartTime = 0; // Variable to store the start time of the movement
-unsigned long durationMillis = 0; // Variable to store the duration in milliseconds
+unsigned long durationMillis = 75; // Default duration in milliseconds (0.075 seconds)
 
 void setup() {
-  myservo.attach(6); // Attach the servo to pin 5
+  myservo.attach(6);  // Attach the servo to pin 6 (change as needed)
   Serial.begin(9600); // Initialize the serial communication
 }
 
@@ -15,27 +15,26 @@ void loop() {
     String command = Serial.readStringUntil('\n'); // Read the incoming command as a string
     
     if (command.startsWith("MOVE")) {
-      // Parse the command to extract target position 
+      // Parse the command to extract target position
       int spaceIndex = command.indexOf(' ');
       if (spaceIndex != -1) {
-        int targetPosition = command.substring(spaceIndex + 1).toInt();
-        pos = targetPosition; // Set the initial position
-        myservo.write(pos); // Move to the initial position
+        int targetPosition = command.substring(5).toInt();
+        while (myservo.read() != targetPosition) {
+          pos = myservo.read(); // Set the initial position
+          if (pos > targetPosition) {
+            myservo.write(pos - 1);
+          } else {
+            myservo.write(pos + 1);
+          }
+          delay(durationMillis);
+        }
       }
-    } else if (command.startsWith("SET_DURATION")) {
+    } else if (command.startsWith("SET_DURATION")) { 
       // Parse the command to set the duration
       int spaceIndex = command.indexOf(' ');
       if (spaceIndex != -1) {
         durationMillis = (unsigned long)(command.substring(spaceIndex + 1).toFloat() * 1000); // Convert seconds to milliseconds
-        movementStartTime = millis(); // Record the start time of the movement
-      } 
-
-   }  
-
-  } 
-  // Check if it's time to stop the servo movement based on the duration
-    if (durationMillis > 0 && millis() - movementStartTime >= durationMillis) {
-      durationMillis = 0; // Reset the duration
-      // You can take additional actions here if needed
+      }
     }
+  }
 }
